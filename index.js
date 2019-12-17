09a53881b0e0518dbf5f3c29fabf14ffb9329b6a
orhuncan <48480813+orhuncan@users.noreply.github.com>
2019-12-17 23:08:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';// aktarım yapılır
-import {name as appName} from './app.json';//aktarım yapılır
+import {name as appName} from './app.json';// aktarım yapılır
 
 AppRegistry.registerComponent(appName, () => App);
